d821dcfa5e0a621cee77480237f2a8bd59e4eac8
JDGrimes <jdg@codesymphony.co>
2017-01-04 16:45:39 -0500

    HTTPS everywhere

--- .wordpoints-dev-lib-config.sh
+++ .wordpoints-dev-lib-config.sh
@@ -5,6 +5,14 @@ shopt -s expand_aliases
 
 # Sets up custom configuration.
 function wordpoints-dev-lib-config() {
+
+	# Ignore some strings that are expected.
+	CODESNIFF_IGNORED_STRINGS=(\
+		"${CODESNIFF_IGNORED_STRINGS[@]}" \
+		# Doesn't support HTTPS.
+		-e wpdatatables.com \
+	)
+
 	alias setup-phpunit="\setup-phpunit; unzip wpdatatables.zip -d /tmp/wordpress/src/wp-content/plugins/"
 }
 
